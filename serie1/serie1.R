bd09da631d11569b641893a31627358c3d001bd8
Oumaima Rd <62358941+lilou1radi@users.noreply.github.com>
2021-11-15 22:45:36 +0000

    Update serie1.R

--- serie1/serie1.R
+++ serie1/serie1.R
@@ -1,16 +1,16 @@
 #===================================exercice1================================
 
-#créer les vecteurs
+#crÃ©er les vecteurs
 Age<-c(22, 25, 18, 20)
 Nom<-c("Ahmed", "Ayoub", "Sara", "Hind")
 Sexe<-c("H", "H", "F", "F")
 
 #///solution 1
 
-#créer le dataframe
+#crÃ©er le dataframe
 df1<-data.frame(Age,Nom,Sexe)
 print(df1)
-#afficher les deux premières lignes du dataframe
+#afficher les deux premiÃ¨res lignes du dataframe
 my_df<-df1[1:2,]
 print(my_df)
 
@@ -22,7 +22,7 @@ subset(DataFrame,Sexe=="H")
 
 #===================================exercice2================================
 
-#créer les vecteurs
+#crÃ©er les vecteurs
 x <- c(2, 4, 6, 8)
 y <- c(TRUE, TRUE, FALSE, TRUE)
 sum(x[y])
@@ -36,7 +36,7 @@ x<-c(10, 20, 30, 25, 9, 26)
 
 #ordre croissant
 x[order(x)]
-#ordre décroissant
+#ordre dÃ©croissant
 x[order(x, decreasing=TRUE)]
 
 #///solution 2:
@@ -55,7 +55,7 @@ x<-c(10, 20, 30, 20, 20, 25, 9, 26)
 table(factor(x))
 
 #///solution 2
-print("la valeur(20) se répète dans le vecteur:")
+print("la valeur(20) se rÃ©pÃ¨te dans le vecteur:")
 print(sum(x==20))
 
 
@@ -63,22 +63,22 @@ print(sum(x==20))
 #===================================exercice5================================
 
 v = c(15,26,9,7,10,0,9,15)
-#test si la valeur d'un élément dans le vecteur v est inférieur à 10
+#test si la valeur d'un Ã©lÃ©ment dans le vecteur v est infÃ©rieur Ã  10
 print(v<10)
 
 
 
 #===================================exercice6================================
-#création des vecteurs
+#crÃ©ation des vecteurs
 v <- c(45:41, 30:33)
 b <- LETTERS[rep(1:3, 3)]
 n <- round(rnorm(9, 65))
 
-#création du dataframe
+#crÃ©ation du dataframe
 df2<-data.frame(v,b,n)
 print(df2)
 
-#Trier le data frame par la première colone.
+#Trier le data frame par la premiÃ¨re colone.
 #///solution 1
 positions<-order(df2$v)
 df2[positions, ]
@@ -89,23 +89,23 @@ df2[with (df2, order(v)),]
 
 
 #===================================exercice7================================
-#création de la matrice
+#crÃ©ation de la matrice
 matrice <- matrix(1:20, ncol = 5)
 print(matrice)
-#création du dataframe
+#crÃ©ation du dataframe
 df <- as.data.frame(matrice)
 print(df)
-#changer les attributs des lignes au id_i(avec i le numéro de ligne) de même pour les colonnes variables_i.
+#changer les attributs des lignes au id_i(avec i le numÃ©ro de ligne) de mÃªme pour les colonnes variables_i.
 colnames(df) <- paste("variable_", 1:ncol(df))
 rownames(df) <- paste("id_", 1:nrow(df))
-#afficher le df pour vérifier les modifications
+#afficher le df pour vÃ©rifier les modifications
 print(df)
 
 
 
 #===================================exercice8================================
 
-#a- vérifier si VADeaths est un Data Frame
+#a- vÃ©rifier si VADeaths est un Data Frame
 is.data.frame(VADeaths)
 #le rendre de type Data frame
 vadeaths<-as.data.frame(VADeaths)
@@ -115,7 +115,7 @@ print(vadeaths)
 total<-rowSums(vadeaths[,-1])
 print(total)
 
-#c- Changer l'ordre des colonnes de telle sorte total est la première colonne.
+#c- Changer l'ordre des colonnes de telle sorte total est la premiÃ¨re colonne.
 cbind(total,vadeaths)
 
 #///autre solution possible 
@@ -127,7 +127,7 @@ vadeaths <- vadeaths[, c(5, 1:4)]
 
 
 #===================================exercice9================================
-#a- vérifier si state.x77 est un Data Frame
+#a- vÃ©rifier si state.x77 est un Data Frame
 is.data.frame(state.x77)
 #le rendre de type Data frame
 stx77<-as.data.frame(state.x77)
@@ -150,27 +150,13 @@ row.names(stx77)[(which(max(stx77$Income) == stx77$Income))]
 #===================================exercice10================================
 
 ma_liste<-list(g1 = 1:10, g2 = "R Programming", g3 = "HTML")
-#ajouter 10 a chaque élément du premier vecteur.
+#ajouter 10 a chaque Ã©lÃ©ment du premier vecteur.
 ma_liste$g1+10
 
 
 
 #===================================exercice11================================
 ma_liste<-list(g1 = 1:10, g2 = "R Programming", g3 = "HTML")
-#affiche tous les éléments du premier vecteur sauf le 3ème
+#affiche tous les Ã©lÃ©ments du premier vecteur sauf le 3Ã¨me
 ma_liste[["g1"]][-3]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
